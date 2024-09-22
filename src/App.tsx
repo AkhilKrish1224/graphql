bfd1fb87e95ece73a7e2714922a99a9d5b150cd0
AkhilKrish1224 <akhilkrishnan1224@gmail.com>
2024-09-23 01:07:07 +0530

    Initial testing with some graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
+import { useEffect } from "react";
 import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+
+export type Launch = {
+  mission_name: string;
+  launch_date_local: string;
+  launch_site: {
+    site_name_long: string;
+  };
+};
+
+const GET_DATA = gql`
+  {
+    launchesPast(limit: 10) {
+      mission_name
+      launch_date_local
+      launch_site {
+        site_name_long
+      }
+    }
+  }
+`;
 
 function App() {
-  return <div className="App"></div>;
+  const { loading, error, data } = useQuery(GET_DATA);
+
+  useEffect(() => {
+    console.log(loading, error, data);
+  }, []);
+  return (
+    <div className="App">
+      {data
+        ? data.launchesPast.map((launch: Launch) => {
+            return (
+              <p>{launch.mission_name + " " + launch.launch_date_local}</p>
+            );
+          })
+        : null}
+    </div>
+  );
 }
 
 export default App;
