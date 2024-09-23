71be78dff5d93bf457ec8fb09376b0069dd7f7c9
AkhilKrish1224 <akhilkrishnan1224@gmail.com>
2024-09-23 22:59:28 +0530

    Added GraphQL Mutation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, useMutation, gql } from "@apollo/client";
 
 export type Customer = {
   id: number;
@@ -18,12 +18,47 @@ const GET_DATA = gql`
   }
 `;
 
+const MUTATE_DATA = gql`
+  mutation MUTATE_DATA($name: String!, $industry: String!) {
+    createCustomer(name: $name, industry: $industry) {
+      customer {
+        id
+        name
+        industry
+      }
+    }
+  }
+`;
+
 function App() {
+  const [name, setName] = useState<string>("");
+  const [industry, setIndustry] = useState<string>("");
+
   const { loading, error, data } = useQuery(GET_DATA);
+  const [
+    createCustomer,
+    {
+      loading: createCustomerLoading,
+      error: createCustomerError,
+      data: createCustomerData,
+    },
+  ] = useMutation(MUTATE_DATA, {
+    refetchQueries: [
+      {
+        query: GET_DATA,
+      },
+    ],
+  });
 
   useEffect(() => {
     console.log(loading, error, data);
-  }, []);
+    console.log(
+      createCustomer,
+      createCustomerLoading,
+      createCustomerData,
+      createCustomerError
+    );
+  });
   return (
     <div className="App">
       {error ? <p>Something went wrong</p> : null}
@@ -35,6 +70,45 @@ function App() {
             );
           })
         : null}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          createCustomer({
+            variables: { name: name, industry: industry },
+          });
+          if (!error) {
+            setName("");
+            setIndustry("");
+          }
+        }}
+      >
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            id="name"
+            type="text"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <label htmlFor="industry">Industry:</label>
+          <input
+            id="industry"
+            type="text"
+            value={industry}
+            onChange={(e) => {
+              setIndustry(e.target.value);
+            }}
+          />
+        </div>
+        <button disabled={createCustomerLoading ? true : false}>
+          Add Customer
+        </button>
+        {createCustomerError ? <p>Error creating customer</p> : null}
+      </form>
     </div>
   );
 }
